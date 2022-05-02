8d361a0901b9cd626902d618b88eacb50e053516
Medion1031 <2001oliver31@gmail.com>
2022-05-02 13:47:09 +0200

    Base file system

---
+++ data/series.php
@@ -0,0 +1,26 @@
+<?php
+
+$series = [
+    'series1' => [
+        'id' => 'series1',
+        'year' => 1987,
+        'title' => 'Egy rém rendes család',
+        'plot' => 'A sorozat főszereplője Al Bundy, egy szerencsétlen papucsférj a korábban sikeres középiskolai amerikaifutball-játékos, aki nőicipő-bolti eladóként dolgozik.',
+        'cover' => 'https://upload.wikimedia.org/wikipedia/hu/thumb/f/f6/MWC_1.jpg/375px-MWC_1.jpg',
+        'episodes' => [
+            '1' => ['id' => '1', 'date' => '1987-04-05', 'title' => 'Új szomszédok', 'plot' => 'Új szomszédok költöznek Al és Peggy mellé, akiknek látszólag tökéletes a házasságuk, a Bundyk azonban gondoskodnak arról, hogy a harmónia gyorsan a múlté legyen.', 'rating' => 7.8],
+            '2' => ['id' => '2', 'date' => '1987-04-12', 'title' => 'Peggy és a stewardesek', 'plot' => 'Marcy ajánlására Peggy egy új étrendet szeretne bevezetni otthon, aminek Al nagyon nem örül.', 'rating' => 6.5]
+        ]
+    ],
+    'series2' => [
+        'id' => 'series2',
+        'year' => 2021,
+        'title' => 'Nyerd meg az életed',
+        'plot' => '456 anyagi problémákkal küszködő ember meghívást kap egy játékba, melynek díja 45,6 milliárd dél-koreai von.',
+        'cover' => 'https://flixpatrol.com/runtime/cache/files/posters/d/ddlemu3ez0pgg93k2svnlcjcsve.webp',
+        'episodes' => [
+            '1' => ['id' => '1', 'date' => '2021-09-17', 'title' => 'Piros lámpa, zöld lámpa', 'plot' => 'Szong Gihun szerencsétlen életet él, ráadásul egy csomó pénzzel tartozik uzsorásoknak is. Egyik nap a metróállomáson egy rejtélyes, jól öltözött férfi egy ttakcsi nevű játékra invitálja őt, amellyel nyer is százezer vont...', 'rating' => 8.9],
+            '2' => ['id' => '2', 'date' => '2021-09-17', 'title' => 'Pokol', 'plot' => 'Csak 201 játékos maradt életben és közülük is többen könyörögnek, hadd távozhassanak. Ez azonban lehetetlen, kivéve ha a játékosok többsége a befejezés mellett voksol..', 'rating' => 7.2]
+        ]
+    ]
+];
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    include './data/series.php'
+    echo($series)
+?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Netflüx</title>
+</head>
+<body>
+    <div id="wrapper">
+
+    </div>
+</body>
+</html>
